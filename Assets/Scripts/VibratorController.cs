79bfc66b125f100166ead964faed809016120cf4
PharoahOfEg <ahmedsamir25jan1995@gmail.com>
2023-04-27 21:56:58 +0200

    Added vibration, vibration controller, vibration on hit

---
+++ Assets/Scripts/VibratorController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RDG;
+public class VibratorController : MonoBehaviour
+{
+
+    static VibratorController instance;
+    public static VibratorController Instance {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject("Vibrator Controller").AddComponent<VibratorController>();
+                return instance;
+            }
+            else
+                return instance;
+        }
+
+    }
+    /// <summary>
+    /// Vibrates
+    /// </summary>
+    public void Vibrate()
+    {
+        //Handheld.Vibrate();
+        Vibration.Vibrate(100,250);
+    }
+    /// <summary>
+    /// Vibrates for secs seconds.
+    /// Not implemented yet!
+    /// </summary>
+    /// <param name="secs"></param>
+    public void Vibrate(float secs)
+    {
+        Vibration.Vibrate((long) secs * 1000);
+    }
+
+    public void Vibrate(long[] pattern, int repeat=-1)
+    {
+
+        Vibration.Vibrate(pattern,repeat: repeat);
+    }
+}
