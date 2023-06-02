dfddc315000e76fd1589f68bd03e79714fd652fc
mahmoudproto <mahmoud_proto@hotmail.com>
2023-06-03 01:01:38 +0300

    diamonds generator script

--- Assets/Scripts/DiamondsGenerator.cs
+++ Assets/Scripts/DiamondsGenerator.cs
@@ -6,20 +6,25 @@ using Random = UnityEngine.Random;
 
 public class DiamondsGenerator : MonoBehaviour
 {
-    [SerializeField]List<GameObject> diamondsPrefaps;
-    [SerializeField] GameObject diamondsSpawnLocations;
+    [SerializeField] List<GameObject> diamondsPrefabs;
+    [SerializeField] GameObject diamondsSpawnLocationsPrefab;
     [SerializeField] int diamondsDistancediffrence;
     List<GameObject[]> diamondsClusterBool;
     GameObject diamondsContainer;
     Transform playerTransform;
+    GameObject diamondsSpawnLocationsParent;
     const int diamondsClusterCount = 3;
+    List<Transform> diamondsSpawnLocationsList;
+    private int previousRandomIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         diamondsContainer = new GameObject("Diamonds Container");
         HazardsGenerator.onNewHazardGenerated += GenerateDiamonds;
-        playerTransform =GameManager.instance.playerTransform;
+        playerTransform = GameManager.instance.playerTransform;
         InitializeDiamondsBool();
+        IntializeDiamondsSpownLocations();
     }
 
     private void InitializeDiamondsBool()
@@ -28,26 +33,64 @@ public class DiamondsGenerator : MonoBehaviour
         GameObject[] tempDiamondsArray;
         const int vertical_distance = 20;
         diamondsClusterBool = new List<GameObject[]>();
-        for (int i = 0; i < diamondsPrefaps.Count; i++)
+        for (int i = 0; i < diamondsPrefabs.Count; i++)
         {
             tempDiamondsArray = new GameObject[3];
             for (int j = 0; j < diamondsClusterCount; j++)
             {
-                tempDiamondPrefab = GameObject.Instantiate(diamondsPrefaps[i], Vector3.zero + new Vector3(0, vertical_distance), Quaternion.identity, diamondsContainer.transform);
+                tempDiamondPrefab = Instantiate(diamondsPrefabs[i], Vector3.zero + new Vector3(0, vertical_distance), Quaternion.identity, diamondsContainer.transform);
                 tempDiamondsArray[j] = tempDiamondPrefab;
             }
             diamondsClusterBool.Add(tempDiamondsArray);
         }
     }
 
-    private void GenerateDiamonds()
+    private void IntializeDiamondsSpownLocations()
     {
-        int randomIndex = Random.Range(0, diamondsClusterBool.Count);
-        for (int i = 0; i < diamondsClusterCount; i++)
+        const int vertical_distance = 20;
+        diamondsSpawnLocationsParent = Instantiate(diamondsSpawnLocationsPrefab, new Vector3(0, vertical_distance), Quaternion.identity);
+        diamondsSpawnLocationsList = new List<Transform>();
+        foreach (Transform child in diamondsSpawnLocationsParent.transform)
         {
-            diamondsClusterBool[randomIndex][i].transform.position = playerTransform.position - new Vector3(0,diamondsDistancediffrence);
+            diamondsSpawnLocationsList.Add(child);
         }
-
     }
 
+    private void GenerateDiamonds()
+    {
+        ShuffleSpawnLocations();
+        diamondsSpawnLocationsParent.transform.position = playerTransform.position - new Vector3(0, diamondsDistancediffrence);
+        int randomIndex = Random.Range(0, diamondsClusterBool.Count);
+        // remove this garbage whenever you can
+        if (previousRandomIndex == randomIndex && (randomIndex == diamondsClusterBool.Count - 1 || randomIndex == 0))
+            randomIndex = 2;
+        else if(previousRandomIndex == randomIndex)
+            randomIndex--;
+        previousRandomIndex = randomIndex;
+        ////////
+        for (int i = 0; i < diamondsClusterCount; i++)
+        {
+            diamondsSpawnLocationsList[i].gameObject.SetActive(true);
+            diamondsClusterBool[randomIndex][i].transform.position = diamondsSpawnLocationsList[i].position;
+        }
+    }
+
+    private void ShuffleSpawnLocations()
+    {
+        System.Random randomObject = new System.Random();
+        int remainingListItemsCount = diamondsSpawnLocationsList.Count;
+        while (remainingListItemsCount > 1)
+        {
+            remainingListItemsCount--;
+            int k = randomObject.Next(remainingListItemsCount + 1);
+            Transform value = diamondsSpawnLocationsList[k];
+            diamondsSpawnLocationsList[k] = diamondsSpawnLocationsList[remainingListItemsCount];
+            diamondsSpawnLocationsList[remainingListItemsCount] = value;
+        }
+        // hide all diamond vines 
+        for (int i = 0; i < diamondsSpawnLocationsList.Count; i++)
+        {
+            diamondsSpawnLocationsList[i].gameObject.SetActive(false);
+        }
+    }
 }
